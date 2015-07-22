a3169d20c8ede64f5147523254aec0aea0a51d72
sai ch <chsai.btech@gmail.com>
2015-07-22 11:00:32 +0530

    fix user authentication bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,17 @@ class SessionsController < ApplicationController
   end  
     
   def create  
-    user = User.authenticate(params[:email], params[:password])  
-    Rails.logger.info "User: #{user.inspect}"
+    user = User.authenticate(params[:username], params[:password])  
+    Rails.logger.info " Session User info: #{user.inspect}"
     if user  
-      session[:user_id] = user.objectId  
-       Rails.logger.info "UserID: #{session[:user_id]}"
+       session[:user_id] = user.objectId  
+       Rails.logger.info "Session UserID: #{session[:user_id]}"
       redirect_to root_url, :notice => "Logged in!"  
     else  
       flash.now.alert = "Invalid email or password"  
       render "new"  
     end  
+
   end 
 
   def destroy  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,28 @@
 class User < ParseUser
 	alias :email :username
 	
+	fields :username, :password
+
+   def self.authenticate(username, password)
+
+      base_uri   = "https://api.parse.com/1/login"
+      app_id     = settings['app_id']
+      master_key = settings['master_key']
+
+      Rails.logger.info "#{username} - #{password} - #{app_id} - #{master_key}"
+
+      resource = RestClient::Resource.new(base_uri, app_id, master_key)
+    
+    begin
+      resp = resource.get(:params => {:username => username, :password => password})
+      user = model_name.to_s.constantize.new(JSON.parse(resp), false)
+            
+      user 
+    rescue 
+      false
+    end
+
+   end
+
+
 end
